24ec6832aa3e67fe49fa473327002cf487d41a9a
89barde <jjk137199@gmail.com>
2021-09-09 20:40:05 +0800

    fix get_client_ip from

--- djangocms_forms/forms.py
+++ djangocms_forms/forms.py
@@ -18,10 +18,12 @@ if DJANGO_VERSION < 20000: # 2.0.0:
     from django.core.urlresolvers import reverse
 else:
     from django.urls import reverse
+    
 try:
-    from ipware.ip import get_client_ip
+    from ipware import get_client_ip
 except ImportError:
     from ipware.ip import get_ip
+    
 from unidecode import unidecode
 
 from .fields import FormBuilderFileField, HoneyPotField, MultipleChoiceAutoCompleteField, ReCaptchaField
